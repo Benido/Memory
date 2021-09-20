bd7de39cd932380facf0c15e7de4cf1d38785aa3
Benido <remi.besnie@gmail.com>
2021-09-20 17:35:16 +0200

    Commit intermédiaire

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-let nombrePaires = document.getElementById('pairs-selection')
-const selection = document.getElementById("selection-theme")
-const cardsGrid = document.getElementById("memory-card-grid")
-const buttonLetsGo = document.getElementById("button-lets-go")
 
 //La page lance une modale au chargement
 $(window).on("load",function(){
-  $("#start-modal").modal("show");
+  $("#start-modal").modal("show")
+  ;
 })
 
-/*//Ecoute du clic de lancement de partie sur la modale
-buttonLetsGo.addEventListener("click", () => {
-  alert('ça marche')
-})
-function test() {
-  alert(`il y a  paires`)
-  console.log(nombrePaires.value)
-}*/
 
-$("#pairs-selection").change(function () {
-  let val = $("#pairs-selection option:selected").val()
-  alert(val)
-})
+const cardsGrid = $("#memory-card-grid")
+const themeSelection = $("#theme-selection")
+const buttonLetsGo = $("#button-lets-go")
+const pairsNumber = $("#pairs-selection")
+const foundPairs = $("#found-pairs-grid-1")
 
 //La zone de jeu est initialisée avec le nombre de cartes correspondant aux paires et au thème choisis
-function createGrid(nbCartes) {  
- alert('coucou' + nbCartes)
- //for (let i=0; i < nbCartes; i++)  {
-   let wrapper = document.createElements("div")
-   wrapper.classList.add("col d-flex justify-content-center")
-   let card = document.createElements("div")
-   card.classList.add("memory-card shadow")
+function createGrid(nbPairs) {   
+
+ for (let i=0; i < nbPairs * 2; i++)  {
+   let wrapper = document.createElement("div")
+   wrapper.classList.add("col")
+   wrapper.classList.add("d-flex")
+   wrapper.classList.add("justify-content-center")
+   let card = document.createElement("div")
+   card.classList.add("memory-card")
+   card.classList.add(themeSelection.val())
+   card.classList.add("shadow")
    wrapper.append(card)
    cardsGrid.append(wrapper)
- //}
+ }
 }
-/*
+
+//La paire trouvée est ajoutée dans la zone du joueur
+function addFoundPairs(card) {
+  foundPairs.html(`<div class ='col d-flex justify-content-center'><div class = ${card}></div></div>`)
+  
+}
+
 // Fisher-Yates Shuffle
 function shuffle(array) {
   var m = array.length, t, i;
@@ -50,11 +50,10 @@ function shuffle(array) {
   return array;
 }
 
-//Le script génère le nombre de paires que le joueur a déterminée, puis mélange les valeurs dans une table.!
-function newTable() {
-  const pairs = nombrePaires.value
+//Génère le nombre de paires que le joueur a déterminée, puis mélange les valeurs dans une table.
+function newTable(nbCartes) {
   let newTable = []
-  for (let i = 1; i <= pairs; i++) {
+  for (let i = 1; i <= nbCartes; i++) {
     newTable.push(i) 
     newTable.push(i)
   }
@@ -62,19 +61,79 @@ function newTable() {
   return newTable
 }
 
-let table = newTable()
-//alert(table)
-
-//Chaque tour, le joueur peut saisir deux chiffres pour tenter d'assembler une paire. 
-function reveal (array) {
-  let chiffre = parseInt(prompt("Choisissez un chiffre entre 1 et " + array.length), 10)
-  while (isNaN(chiffre) && chiffre > (array.length) && chiffre < 1) {
-    chiffre = parseInt(prompt("Merci d'entrer un chiffre entre 1 et " + array.length), 10)
-  }        
-  alert(`Vous dévoilez le nombre ${array[chiffre-1]}`)
-  return chiffre
+//Attribue à chaque carte une valeur de la table
+function giveNumber(randomizedTable) {
+  for (let i = 0; i < randomizedTable.length; i++) {  
+  $("#memory-card-grid > div > .memory-card").eq(i).addClass(`f${randomizedTable[i]}`)
+  }
 }
 
+//A la fermeture de la modale de lancement, la grille de carte est initialisée
+$("#button-lets-go").on("click", function () {
+  createGrid(pairsNumber.val())
+  let table = newTable(pairsNumber.val())
+  giveNumber(table)
+  partyBegins()
+})
+
+
+//Chaque tour, le joueur peut cliquer sur deux cartes pour tenter d'assembler une paire. 
+
+//Le clic révèle la face de la carte. Le nombre de clic est limité à 2
+function revealCards(counter, array) { 
+  $("#memory-card-grid").one("click", ".memory-card", (function() {
+    $(this).removeClass(`${themeSelection.val()}`)
+      let carte = $(this)
+      array.push(carte) 
+      counter++      
+      if (counter < 2) {
+        revealCards(counter, array)
+      }   
+      else if (array[0].attr("class") === array[1].attr("class")) {        
+       setTimeout(function() {
+        $(array[0]).hide()
+        $(array[1]).hide()
+        addFoundPairs($(this).attr("class"))
+        realPairsCounter--
+       }, 1000)    
+       return     
+      } else {
+        setTimeout(function() {        
+        $(array[0]).addClass(`${themeSelection.val()}`)
+        $(array[1]).addClass(`${themeSelection.val()}`)
+        }, 1000)  
+        return
+        }
+    }))
+}
+
+
+let i = 0
+let compare = []
+let realPairsCounter
+
+function partyBegins () {
+  realPairsCounter = pairsNumber.val()
+  while (realPairsCounter > 0) {
+    revealCards(i, compare)
+  }
+
+/*  if (realPairsCounter > 0) {
+    revealCards(i, compare, realPairsCounter)
+  }*/
+    
+  
+}
+
+
+
+
+
+
+
+
+
+/*
 function newTurn(array) {
   let index1 = reveal(array) -1
   let index2 = reveal(array) -1
